933aa631da3769060f71186d4d47283f484c0d34
anesanika <nikaanesiani06@gmail.com>
2025-04-03 09:11:02 -0700

    still fixing

--- src/app/req/axios.ts
+++ src/app/req/axios.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = process.env.NEXT_PUBLIC_API_URL || "http://127.0.0.1:8000";
+const baseURL = process.env.NEXT_PUBLIC_API_URL;
 const db = axios.create({
   baseURL,
   headers: {
