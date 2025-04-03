1ef16324f1c0af01baa2b5d8e9b067989e75644e
anesanika <nikaanesiani06@gmail.com>
2025-04-03 05:09:26 -0700

    commit of

--- src/app/req/axios.ts
+++ src/app/req/axios.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
+const baseURL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000/";
 const db = axios.create({
-  baseURL: "http://127.0.0.1:8000/",
+  baseURL,
   headers: {
     "Content-Type": "application/json",
   },
