d8729d207e730739f2d7c307be857ac96a95782a
anesanika <nikaanesiani06@gmail.com>
2025-04-03 05:17:18 -0700

    update anothertime

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,16 @@ import Prodcuts from "./components/products/Products";
 import db from "./req/axios";
 
 export default async function Home() {
-  const { data } = await db.get("/store/products/");
+  try {
+    const { data } = await db.get("/store/products/");
 
-  return (
-    <Suspense>
-      <Prodcuts product={data} />
-    </Suspense>
-  );
+    return <Prodcuts product={data} />;
+  } catch (e) {
+    return (
+      <main>
+        <h1>Welcome to my app</h1>
+        <p>Sorry, we couldn't load the latest data.</p>
+      </main>
+    );
+  }
 }
