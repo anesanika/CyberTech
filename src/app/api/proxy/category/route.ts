ce559416dc7b15b73cf247f6301089b15bd241bd
anesanika <nikaanesiani06@gmail.com>
2025-04-18 07:59:28 -0700

    fix

--- src/app/api/proxy/category/route.ts
+++ src/app/api/proxy/category/route.ts
@@ -4,7 +4,6 @@ import { NextResponse } from "next/server";
 export async function GET() {
   try {
     const { data } = await db.get("/store/category");
-
     return NextResponse.json(data);
   } catch (error) {
     console.error("Error fetching categories:", error);
